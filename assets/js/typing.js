5b61f4802c2087a6f5ed0dd8711c03a52754ec22
Bagas Aria Sativa <bagasativa@gmail.com>
2026-02-01 10:49:02 +0700

    Add files via upload

--- assets/js/typing.js
+++ assets/js/typing.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const options = {
         strings: [
             'Full Stack Web Developer',
-            'Laravel & CodeIgniter Expert',
+            'Laravel &amp; CodeIgniter Expert',
             'Manufacturing ERP Specialist',
             'React JS Developer',
             'Database Designer'
@@ -11,7 +11,8 @@ document.addEventListener('DOMContentLoaded', function() {
         backSpeed: 30,
         backDelay: 2000,
         loop: true,
-        showCursor: false
+        showCursor: false,
+        smartBackspace: true
     };
 
     const typed = new Typed('#typed-text', options);
